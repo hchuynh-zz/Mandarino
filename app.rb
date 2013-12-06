88156535c1bad23fd54fa5417e3e36d53789c862
Hoang C. Huynh <hoangchau.huynh@gmail.com>
2013-12-06 09:41:10 +0100

    test variabile

--- app.rb
+++ app.rb
@@ -61,11 +61,13 @@ helpers do
   # HH
 
   def getMandarini
+    message = "DAJE!"
+    return message
   end
 
   def writeMandarini(id, day, howmany = 0)
   end
-  
+
 end
 
 # the facebook session expired! reset ours and restart the process
@@ -81,12 +83,12 @@ get "/" do
   # Get public details of current application
   @app  =  @graph.get_object(ENV["FACEBOOK_APP_ID"])
 
+  @message = "Quanti #mandarini hai mangiato oggi?"
+
   if access_token
     @user    = @graph.get_object("me")
     @friends = @graph.get_connections('me', 'friends')
-    #@photos  = @graph.get_connections('me', 'photos')
-    #@likes   = @graph.get_connections('me', 'likes').first(4)
-
+    @message = getMandarini
     # for other data you can always run fql
     @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   end
