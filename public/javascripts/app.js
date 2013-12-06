13409a90b2add5edf0b112f5ba95433630d59d6d
Hoang C. Huynh <hoangchau.huynh@gmail.com>
2013-12-06 19:12:24 +0100

    test submit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -64,18 +64,35 @@
         popup.init();
 
 
-        $("#howmany").keypress(function(e){
-            var theEvent = e || window.event;
-            var key = theEvent.keyCode || theEvent.which;
-            key = String.fromCharCode( key );
-            var regex = /[0-9]|\./;
-             console.log(key);
-            if (key == 13) {
+        $("#howmany").keypress(function(evt){
+              var e = evt || window.event;
+              var key = e.keyCode || e.which;
+
+              if (key == 13){
                 $("#theForm").submit();
-            }
-            if( !regex.test(key) ) {
-              if(theEvent.preventDefault) theEvent.preventDefault();
-            }
+              }
+              
+              if (!e.shiftKey && !e.altKey && !e.ctrlKey &&
+              // numbers   
+              key >= 48 && key <= 57 ||
+              // Numeric keypad
+              key >= 96 && key <= 105 ||
+              // Backspace and Tab and Enter
+              key == 8 || key == 9 || key == 13 ||
+              // Home and End
+              key == 35 || key == 36 ||
+              // left and right arrows
+              key == 37 || key == 39 ||
+              // Del and Ins
+              key == 46 || key == 45) {
+                  // input is VALID
+              }
+              else {
+                  // input is INVALID
+                  e.returnValue = false;
+                  if (e.preventDefault) e.preventDefault();
+              }
+          }
 
         }).focus(function(){
           $(this).select();
