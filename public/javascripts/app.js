37c24a70bd161cb1502f904c7947f73ec3bffedb
Hoang C. Huynh <hoangchau.huynh@gmail.com>
2013-12-06 09:14:31 +0100

    Aggiunto SCSS

---
+++ public/javascripts/app.js
@@ -0,0 +1,84 @@
+      function logResponse(response) {
+        if (console && console.log) {
+          console.log('The response was', response);
+        }
+      }
+
+      $(function(){
+        // Set up so we handle click on the buttons
+        $('#postToWall').click(function() {
+          FB.ui(
+            {
+              method : 'feed',
+              link   : $(this).attr('data-url')
+            },
+            function (response) {
+              // If response is null the user canceled the dialog
+              if (response != null) {
+                logResponse(response);
+              }
+            }
+          );
+        });
+/*
+        $('#sendToFriends').click(function() {
+          FB.ui(
+            {
+              method : 'send',
+              link   : $(this).attr('data-url')
+            },
+            function (response) {
+              // If response is null the user canceled the dialog
+              if (response != null) {
+                logResponse(response);
+              }
+            }
+          );
+        });
+*/
+        $('#sendRequest').click(function() {
+          FB.ui(
+            {
+              method  : 'apprequests',
+              message : $(this).attr('data-message')
+            },
+            function (response) {
+              // If response is null the user canceled the dialog
+              if (response != null) {
+                logResponse(response);
+              }
+            }
+          );
+        });
+      });
+
+
+      $(function() {
+
+        $("#theInput").keypress(function(e){
+            var theEvent = e || window.event;
+            var key = theEvent.keyCode || theEvent.which;
+            key = String.fromCharCode( key );
+            var regex = /[0-9]|\./;
+            if( !regex.test(key) ) {
+              theEvent.returnValue = false;
+              if(theEvent.preventDefault) theEvent.preventDefault();
+            }
+        });
+
+        $("#mandarinosubmit").submit(function(e){
+          e.preventDefault();
+
+          $.ajax({
+            type: "POST",
+            url: "/register",
+            data: $("#mandarinosubmit").serialize(),
+            success: function(){
+              $("#message").html("Successfully registered")
+            },
+            error: function(){
+              $("#message").html("Not Successful")
+            }
+          });
+        });
+      });
